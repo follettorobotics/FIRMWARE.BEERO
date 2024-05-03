85a619cb3d51aa5598d33107eb1b9adc637c9035
unknown <crossedfinger-j>
2024-05-03 18:13:24 +0900

    .

---
+++ TCPHandler.cpp
@@ -0,0 +1,50 @@
+#include "TCPHandler.h"
+
+void TCPHandler::clientHandle(){
+    EthernetClient newClient = TCPserver.available();
+
+    if (newClient && !TCPclient.connected()){
+        TCPclient = newClient;  // over-write TCPclient
+        Serial.println("tcp client disconnected and new client accept");
+    }
+
+    // parse the message logic 
+    messageHandle(); 
+
+    return;
+}
+
+void TCPHandler::messageHandle(){
+    byte request[20];
+    size_t requestSize = 0;
+
+    // check the data 
+    // if the client disconnected, client.available() return 0
+    int dataSize = TCPclient.available();
+    if (dataSize > 0){
+        for (int i=0; i<dataSize; i++){
+            byte data = TCPclient.read();
+            request[requestSize++] = data; 
+        }
+    }
+   
+    Dispatcher& dispatch = Dispatcher::getInstance();
+
+}
+
+bool TCPHandler::sendMessageToClient(const byte* message, size_t size){
+    if (TCPclient.connected()){
+        TCPclient.write(message, size);
+        return true; 
+    }else{
+        EthernetClient newClient = TCPserver.available();
+        if (newClient){
+            TCPclient = newClient;
+            TCPclient.write(message, size); 
+            return true; 
+        }
+        else{
+            return false; 
+        }
+    }
+}
---
+++ TCPHandler.h
@@ -0,0 +1,27 @@
+#ifndef TCPHANDLER_H
+#define TCPHANDLER_H
+
+#include <Ethernet.h>
+
+
+// TCPHandler singleton pattern 
+class TCPHandler{
+private:
+
+    TCPHandler() : TCPserver(TCPHandler::port) {}
+    TCPHandler(const TCPHandler&) = delete;
+    TCPHandler& operator=(const TCPHandler&) = delete;
+
+    EthernetServer TCPserver;
+    EthernetClient TCPclient; 
+    static uint16_t port;
+
+public:
+
+    static TCPHandler& getInstance();
+    void clientHandle(); 
+    void messageHandle();
+    bool sendMessageToClient(const byte *message, size_t size);
+};
+
+#endif
