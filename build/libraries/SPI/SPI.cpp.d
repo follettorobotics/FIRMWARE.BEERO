a2642cba3e3748724fae511279403f7b9a59315e
unknown <crossedfinger-j>
2024-07-02 13:34:47 +0900

    .

--- build/libraries/SPI/SPI.cpp.d
+++ build/libraries/SPI/SPI.cpp.d
@@ -1,14 +1,14 @@
 C:\Users\Owner\OneDrive\πŸ≈¡\ »≠∏È\FIRMWARE.BEERO\build\libraries\SPI\SPI.cpp.o: \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\libraries\SPI\src\SPI.cpp \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\libraries\SPI\src\SPI.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/binary.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/WCharacter.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/WString.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/HardwareSerial.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Stream.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Print.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Printable.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/USBAPI.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
- C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.5\variants\mega/pins_arduino.h
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\SPI\src\SPI.cpp \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\SPI\src\SPI.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WCharacter.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/WString.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/HardwareSerial.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Stream.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Print.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Printable.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/USBAPI.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
+ C:\Users\Owner\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\mega/pins_arduino.h
